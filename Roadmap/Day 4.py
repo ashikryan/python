dda4704d1bb49597fa32e9022b09170f8f59e66c
Ash <youremail@example.com>
2026-06-22 12:16:56 +0530

    Day 4

--- Roadmap/Day 4.py
+++ Roadmap/Day 4.py
@@ -1,5 +1,4 @@
 #---------------------------------   >>  Loops  <<   ------------------------------------------#
-
 #----> for loop
 
 i = 20 
@@ -46,8 +45,41 @@ for i in range(1,6):
     total += i
     print(total)
 
+#########------------------##########
+
+count = 0
+while count<=5:
+    print(count)
+    count+=1
 
 
+for i in range(6):
+    print(i)
 
 
+for a in range(0,6):
+    print(a)
 
+
+for p in range(0,11,2):
+    print(p)
+
+#---------------------------->>>>   step reverse 
+for r in range(10,0,-1):
+    print(r)
+
+
+#------------ project -------------#
+#---------------------------------#
+secret = 7
+
+guess= int(input("enter your guess:"))
+
+while guess != secret:
+    print("wrong guess")
+
+    guess= int(input("try again:"))
+
+print("you win")
+
+ #--------------------------------#
