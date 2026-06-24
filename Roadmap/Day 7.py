c576be9248aef8678f160f0410d2e3b1628d140d
Ash <youremail@example.com>
2026-06-24 18:43:42 +0530

    Day 6

---
+++ Roadmap/Day 7.py
@@ -0,0 +1,12 @@
+# tuple vs Lust
+
+student = ("rahul", "amal")  #Tuple
+name = ["rahul","amal"]   #List
+
+#____________name[0] = "john"        # list allowed 
+#____________student[0] = "john"     # Tuple not allowed
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> #
+
+
+#mvc arch , css (2) embedd sytle sheet , intrn extrnl shet , web 2.0 ,cookie defi 
