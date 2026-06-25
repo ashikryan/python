73e9d609959336c2029a447c72e55f5c4fcf524d
Ash <youremail@example.com>
2026-06-25 18:48:37 +0530

    Day 7

--- Roadmap/Day 7.py
+++ Roadmap/Day 7.py
@@ -1,12 +1,41 @@
 # tuple vs Lust
 
 student = ("rahul", "amal")  #Tuple
-name = ["rahul","amal"]   #List
+names = ["rahul","amal"]   #List
 
 #____________name[0] = "john"        # list allowed 
 #____________student[0] = "john"     # Tuple not allowed
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> #
+#Dictionary______________________
+
+members = {
+    "name":"Ryan",
+    "years": '5',
+    "marks":'50'
+}
+
+#_____________________________Access Value:
+
+print(members["name"])
+print(members["years"])
+print(members["marks"])
+
+#_____________________________Update & Add New Value
+
+members["marks"] = 60
+members ["city"] = "Goa"
 
 
-#mvc arch , css (2) embedd sytle sheet , intrn extrnl shet , web 2.0 ,cookie defi 
+#-------------------- loop through -----------------
+for x in members:    # keys
+    print(x)
+
+for x in members:    #values
+    print(members[x])
+
+#---------------------------------------------------
+values = [1,2,3,4,4,5,5,5,5,5,4,4,3,3,4,4]
+unique = set (values)    
+
+print(unique)   
